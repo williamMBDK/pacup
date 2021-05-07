241bcdd77b51b4c10433b5a9f0cd91c5f3fdadcb
williamBMDK <williambillemeyling@gmail.com>
2021-05-07 13:07:31 +0200

    fix getter for apt

--- lib/apt/get.sh
+++ lib/apt/get.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 packages=( $(comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)) )
 for package in ${packages[@]}; do
-	version=$(apt-cache show $package | grep "Version" | head -n 1)
+	s=$(apt-cache show $package | grep "Version" | head -n 1)
+	regex="Version: (.*)"
+	[[ $s =~  $regex ]]
+	version=${BASH_REMATCH[1]}
 	echo "p:$package, v:$version"
 done
