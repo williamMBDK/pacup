ddae0443ddf868f23aba999e6af7590d787d5e01
williamBMDK <williambillemeyling@gmail.com>
2021-05-07 12:40:15 +0200

    adds yarn getter

---
+++ lib/yarn/get.sh
@@ -0,0 +1,8 @@
+regex="^info \"(.*)@(.*)\""
+items=$(yarn global list 2> /dev/null | grep -P "info")
+echo "$items" | while read line ; do
+	[[ $line =~ $regex ]]
+	package="${BASH_REMATCH[1]}"
+	version="${BASH_REMATCH[2]}"
+	echo p:$package, v:$version
+done
