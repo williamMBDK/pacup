1d151c62aa3f2743143187d1690ac9ec765a63ff
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-13 00:03:16 +0200

    change to getopt

--- lib/install.sh
+++ lib/install.sh
@@ -11,21 +11,40 @@ CONFIG=0
 PACLIST=0
 PACMANAGER=0
 
-while getopts "c:l:p:q" opt; do
-    case $opt in
-        c)
-            CONFIG=$OPTARG
+PARSED_ARGUMENTS=$(getopt -n pacback-install -o c:l:p:q --long configuration,package-list,package-manager:,quiet: -- "$@")
+
+eval set -- "$PARSED_ARGUMENTS"
+
+while :
+do
+    case $1 in
+        -c | --configuration)
+            CONFIG=$2
+            shift 2
             ;;
-        l)
-            PACLIST=$OPTARG
+        -l | --package-list)
+            PACLIST=$2
+            shift 2
             ;;
-        p)
-            PACMANAGER=$OPTARG
+        -p | --package-manager)
+            PACMANAGER=$2
+            shift 2
             ;;
-        q)
+        -q | --quiet)
             QUIET=1
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
     esac
 done
+
+echo Config: $CONFIG
+echo Paclist: $PACLIST
+echo Manager: $PACMANAGER
+echo Quiet: $QUIET
 
 $SCRIPT_DIR/config-match.py -q -c $CONFIG -p $PACLIST | while read -r package ; do
     if test $QUIET -eq 0; then
