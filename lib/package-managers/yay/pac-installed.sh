c6433a8f7796b89b2656d70b22ff9a367b9ab1cc
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-13 03:39:59 +0200

    makes pac-installed fast

--- lib/package-managers/yay/pac-installed.sh
+++ lib/package-managers/yay/pac-installed.sh
@@ -4,7 +4,13 @@ pac=$1
 version=$2
 
 if [ "$#" -eq 1 ]; then
-    yay -Qu $pac | grep $pac && exit 1
+    cachedir="$HOME/.cache/pacback/yay"
+    mkdir -p $cachedir
+    if [[ $(find "$cachedir/yayqu.out" -newermt '-3 seconds') == "" ]]; then
+        touch $cachedir/yayqu.out
+        yay -Qu > $cachedir/yayqu.out
+    fi
+    cat $cachedir/yayqu.out | grep "^$pac " > /dev/null && exit 1
     exit 0
 else
     $SCRIPT_DIR/get.sh | grep "^$pac $version$" > /dev/null
