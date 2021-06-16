978ea005c08e4ae90ac1fc86e43a76d116cd6ad6
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-16 22:20:43 +0200

    sort list on str and add append

--- lib/configuration/tagged_package_list.py
+++ lib/configuration/tagged_package_list.py
@@ -21,6 +21,7 @@ class TaggedPackageList:
 
     def __str__(self):
         assert(self.has_been_initialized)
+        self.tagged_packages.sort()
         return "\n".join(map(str, self.tagged_packages))
     
     def get_tag_map(self) -> dict[str, set[Package]]:
@@ -35,6 +36,20 @@ class TaggedPackageList:
     def contains(self, package : Package):
         # can be optimized
         return package in self.tagged_packages
+    
+    def write_to_file(self, filename : str):
+        with open(filename, 'w') as file:
+            file.write(str(self))
+
+    def append(self, tagged_package : TaggedPackage):
+        # todo?: this can be done faster
+        for package in self.tagged_packages:
+            if package.name == tagged_package.name and package.version == tagged_package.version:
+                raise ValueError("content contains duplicate package@version")
+        self.tagged_packages.append(tagged_package)
+
+    def append_str(self, string : str):
+        self.append(PackageFactory.create_tagged_package_from_string(string))
 
 class TaggedPackageListFactory:
     @staticmethod
