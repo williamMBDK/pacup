25359be5a3bc5eef6e4d84867ef288e6446d0198
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-17 17:43:37 +0200

    make version optional in status script

--- lib/status.sh
+++ lib/status.sh
@@ -9,9 +9,10 @@ SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 CONFIG=0
 PACLIST=0
 PACMANAGER=0
+WITH_VERSION=0
 
 # get arguments
-PARSED_ARGUMENTS=$(getopt -n pacup-status -o c:l:p: --long configuration:,package-list:,package-manager: -- "$@")
+PARSED_ARGUMENTS=$(getopt -n pacup-status -o c:l:p:v --long configuration:,package-list:,package-manager:with-version -- "$@")
 eval set -- "$PARSED_ARGUMENTS"
 while :; do
     case $1 in
@@ -26,6 +27,10 @@ while :; do
         -p | --package-manager)
             PACMANAGER=$2
             shift 2
+            ;;
+        -v|--with-version)
+            WITH_VERSION=1
+            shift
             ;;
         --)
             shift
@@ -45,11 +50,20 @@ exit_on_invalid_package_manager $PACMANAGER
 # get matching packages
 get_matches_and_handle_errors $CONFIG $PACLIST # set $matches
 
+function format_packageversion {
+    packageversion="$1"
+    if [ $WITH_VERSION = 0 ]; then
+        get_packageversion_human_format_name "$packageversion"
+    else
+        get_packageversion_human_format "$packageversion"
+    fi
+}
+
 # check if matching packages are installed
 (IFS=$'\n'
 for packageandversion in $matches; do
     if ! $SCRIPT_DIR/package-managers/$PACMANAGER/pac-installed.sh $packageandversion; then
-        print_colored "YELLOW" "NOT INSTALLED OR UP-TO-DATE: $(get_packageversion_human_format "$packageandversion")"
+        print_colored "YELLOW" "NOT INSTALLED OR UP-TO-DATE: $(get_packageversion_human_format "$packageandversion")" # version is not optional here since version is part of the config
     fi
 done)
 
@@ -60,8 +74,8 @@ packages_in_paclist=$($SCRIPT_DIR/run_module.sh "configuration.get_packages_in_l
 (IFS=$'\n'
 for packageandversion in $explicits; do
     if ! is_package_in_list "$packages_in_paclist" "$packageandversion"; then
-        print_colored "CYAN" "INSTALLED BUT NOT IN PACKAGE LIST: $(get_packageversion_human_format "$packageandversion")"
+        print_colored "CYAN" "INSTALLED BUT NOT IN PACKAGE LIST: $(format_packageversion "$packageandversion")"
     elif ! is_package_in_list "$matches" "$packageandversion"; then
-        print_colored "BLUE" "INSTALLED, IN PACKAGE LIST BUT NOT IN CONFIG: $(get_packageversion_human_format "$packageandversion")"
+        print_colored "BLUE" "INSTALLED, IN PACKAGE LIST BUT NOT IN CONFIG: $(format_packageversion "$packageandversion")"
     fi
 done)
