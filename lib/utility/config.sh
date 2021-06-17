a86904fe536d5c69a7e96e90433d1d874d0e77d2
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-17 23:26:19 +0200

    add methods for getting configs/lists

--- lib/utility/config.sh
+++ lib/utility/config.sh
@@ -9,3 +9,19 @@ fi
 if [[ -z "${PACUP_CONFIGS_DIR}" ]]; then
     PACUP_CONFIGS_DIR="~/.config/pacup/configs/"
 fi
+
+function get_list_dir {
+    printf "$PACUP_LISTS_DIR"
+}
+
+function get_configs_dir {
+    printf "$PACUP_CONFIGS_DIR"
+}
+
+function get_list_for_package_manager {
+    printf "$(get_list_dir)/$1"
+}
+
+function get_config_for_package_manager {
+    printf "$(get_configs_dir)/$1"
+}
