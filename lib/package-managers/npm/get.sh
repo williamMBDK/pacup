fb5b95d63cfcc14ee170c0e2da67de7f2ca9868c
William Bille Meyling <williambillemeyling@gmail.com>
2021-06-18 02:39:27 +0200

    fixes get scritp for npm

--- lib/package-managers/npm/get.sh
+++ lib/package-managers/npm/get.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-regex="(\+--|\`--|└──|├──) (.*)@(.*)"
-output=$(npm list -g -depth=0 | tail -n +2 | head -n -1)
-echo "$output" | while read line ; do
-	[[ $line =~ $regex ]]
-	package="${BASH_REMATCH[2]}"
-	version="${BASH_REMATCH[3]}"
-	echo "$package $version"
+#!/bin/sh
+
+dir=$(npm root -g 2> /dev/null)
+packages="$(ls $dir)"
+
+IFS=$'\n'
+for package in $packages; do
+    pacdir="$dir/$package"
+    version="$(jq -r '.version' $pacdir/package.json)"
+    echo "$package $version"
 done
