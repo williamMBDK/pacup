5f7f7125c7523e99ba6677a08b7fdae6b08ee4d7
William Bille Meyling <williambillemeyling@gmail.com>
2022-03-08 12:01:28 +0100

    dont rely on termcolor

--- lib/util/io.py
+++ lib/util/io.py
@@ -1,5 +1,19 @@
-from termcolor import colored
 import os
+
+colorcodes = {
+    "header" : '\033[95m',
+    "blue" : '\033[94m',
+    "cyan" : '\033[96m',
+    "green" : '\033[92m',
+    "yellow" : '\033[93m',
+    "red" : '\033[91m',
+    "bold" : '\033[1m',
+    "underline" : '\033[4m',
+    "reset" : '\033[0m'
+}
+
+def colored(text, color):
+    return colorcodes[color] + text + colorcodes["reset"]
 
 class PacupUserError(Exception):
     pass
