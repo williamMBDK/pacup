9e2e7e33586362abe451fa42c0b3116fc60c41e7
William Bille Meyling <williambillemeyling@gmail.com>
2022-03-10 01:13:23 +0100

    explicit yay

--- lib/package_managers/yay/install.sh
+++ lib/package_managers/yay/install.sh
@@ -4,6 +4,5 @@ if [ "$#" -eq 2 ]; then
     echo "version not supported by yay"
     exit 1
 else
-    # yay --asexplicit -S "$1"
-    yay --noconfirm -S "$1"
+    yay --noconfirm --asexplicit -S "$1"
 fi
